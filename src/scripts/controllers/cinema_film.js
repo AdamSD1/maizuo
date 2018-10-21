e664a9f16b7770fc7772afcfe520db8bb3c2a889
Adam <2085650098@qq.com>
2018-10-21 21:58:35 +0800

    aa

--- src/scripts/controllers/cinema_film.js
+++ src/scripts/controllers/cinema_film.js
@@ -1,7 +1,7 @@
 //cinema_film的控制层
 //引入cinema_film的控制层和视图层并在此合并 暴露接口
 // 引入模块
-import cinema_film from "../views/cinema_film.html"
+import cinema_film from "../views/git .html"
 import cinema_film_model from "../models/cinema_film"
 
 const render = ()=>{
